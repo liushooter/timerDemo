28556aa2710cd0999a74d57de58459a49ab68fa0
shooter <shooter.hero@gmail.com>
2014-10-03 20:14:17 +0800

    init  project

---
+++ timerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CounterViewController
+//  timerDemo
+//
+//  Created by shooter on 10/3/14.
+//  Copyright (c) 2014 shooter. All rights reserved.
+//
+
+import UIKit
+
+class CounterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
